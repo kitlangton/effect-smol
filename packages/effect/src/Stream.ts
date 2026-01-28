26e9ee538c7b8ddce2651a8226db9f9e67a80c3c
Kit Langton <kit.langton@gmail.com>
2026-01-28 15:02:41 -0500

    TIM-293: clarify zipLatestAll JSDoc (#245)

--- packages/effect/src/Stream.ts
+++ packages/effect/src/Stream.ts
@@ -3041,8 +3041,34 @@ export const zipWithPreviousAndNext = <A, E, R>(
   })
 
 /**
+ * Zips multiple streams so that when a value is emitted by any stream, it is
+ * combined with the latest values from the other streams to produce a result.
+ *
+ * Note: tracking the latest value is done on a per-array basis. That means
+ * that emitted elements that are not the last value in arrays will never be
+ * used for zipping.
+ *
+ * @example
+ * ```ts
+ * import { Console, Effect, Stream } from "effect"
+ *
+ * const stream = Stream.zipLatestAll(
+ *   Stream.make(1, 2, 3).pipe(Stream.rechunk(1)),
+ *   Stream.make("a", "b", "c").pipe(Stream.rechunk(1)),
+ *   Stream.make(true, false, true).pipe(Stream.rechunk(1))
+ * )
+ *
+ * const program = Effect.gen(function*() {
+ *   const result = yield* Stream.runCollect(stream)
+ *   yield* Console.log(result)
+ * })
+ *
+ * Effect.runPromise(program)
+ * // Output: [ [ 1, "a", true ], [ 2, "a", true ], [ 3, "a", true ], [ 3, "b", true ], [ 3, "c", true ], [ 3, "c", false ], [ 3, "c", true ] ]
+ * ```
+ *
  * @since 2.0.0
- * @category zipping
+ * @category Zipping
  */
 export const zipLatestAll = <T extends ReadonlyArray<Stream<any, any, any>>>(
   ...streams: T
