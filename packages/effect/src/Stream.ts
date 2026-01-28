bf022cfa0499b21ede9809849aee72a8091d3e0f
Kit Langton <kit.langton@gmail.com>
2026-01-28 14:58:08 -0500

    TIM-339: improve Stream.rechunk JSDoc (#289)
    * TIM-339: improve Stream.rechunk JSDoc
    
    * TIM-339: tweak Stream.rechunk doc wording

--- packages/effect/src/Stream.ts
+++ packages/effect/src/Stream.ts
@@ -4699,8 +4699,29 @@ export const chunks = <A, E, R>(self: Stream<A, E, R>): Stream<Arr.NonEmptyReado
   )
 
 /**
+ * Re-chunks the stream into arrays of the specified size, preserving element order.
+ *
+ * The size is clamped to at least 1.
+ *
+ * @example
+ * ```ts
+ * import { Console, Effect, Stream } from "effect"
+ *
+ * const program = Effect.gen(function*() {
+ *   const result = yield* Stream.make(1, 2, 3, 4, 5).pipe(
+ *     Stream.rechunk(2),
+ *     Stream.chunks,
+ *     Stream.runCollect
+ *   )
+ *   yield* Console.log(result)
+ * })
+ *
+ * Effect.runPromise(program)
+ * // Output: [ [ 1, 2 ], [ 3, 4 ], [ 5 ] ]
+ * ```
+ *
  * @since 2.0.0
- * @category utils
+ * @category Grouping
  */
 export const rechunk: {
   (size: number): <A, E, R>(self: Stream<A, E, R>) => Stream<A, E, R>
