407716a976b3812bddb7de3bbbe56cda2b472f45
Kit Langton <kit.langton@gmail.com>
2026-01-28 12:44:50 -0500

    TIM-335: improve Stream.dropWhile JSDoc

--- packages/effect/src/Stream.ts
+++ packages/effect/src/Stream.ts
@@ -4577,11 +4577,26 @@ export const dropUntilEffect: {
   ))
 
 /**
- * Drops all elements of the stream for as long as the specified predicate
- * evaluates to `true`.
+ * Drops elements from the stream while the specified predicate evaluates to `true`.
+ *
+ * @example
+ * ```ts
+ * import { Console, Effect, Stream } from "effect"
+ *
+ * const program = Effect.gen(function*() {
+ *   const values = yield* Stream.make(1, 2, 3, 4, 5).pipe(
+ *     Stream.dropWhile((n) => n < 3),
+ *     Stream.runCollect
+ *   )
+ *   yield* Console.log(values)
+ * })
+ *
+ * Effect.runPromise(program)
+ * // Output: [ 3, 4, 5 ]
+ * ```
  *
  * @since 2.0.0
- * @category utils
+ * @category Filtering
  */
 export const dropWhile: {
   <A>(predicate: (a: NoInfer<A>, index: number) => boolean): <E, R>(self: Stream<A, E, R>) => Stream<A, E, R>
