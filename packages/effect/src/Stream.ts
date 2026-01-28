ec4e00c2507b53e21e4bb72f956446cc2005dcf1
Kit Langton <kit.langton@gmail.com>
2026-01-28 13:13:11 -0500

    TIM-345: align Stream.mapAccum JSDoc with spec

--- packages/effect/src/Stream.ts
+++ packages/effect/src/Stream.ts
@@ -4969,8 +4969,31 @@ export const combineArray: {
   )))
 
 /**
+ * Statefully maps over the elements of this stream to produce new elements.
+ *
+ * @example
+ * ```ts
+ * import { Console, Effect, Stream } from "effect"
+ *
+ * const program = Effect.gen(function*() {
+ *   const totals = yield* Stream.make(0, 1, 2, 3, 4, 5, 6).pipe(
+ *     Stream.mapAccum(() => 0, (total, n) => {
+ *       const next = total + n
+ *       return [next, [next]] as const
+ *     }),
+ *     Stream.runCollect
+ *   )
+ *
+ *   yield* Console.log(totals)
+ * })
+ *
+ * Effect.runPromise(program)
+ * // Output:
+ * // [ 0, 1, 3, 6, 10, 15, 21 ]
+ * ```
+ *
  * @since 2.0.0
- * @category sequencing
+ * @category Mapping
  */
 export const mapAccum: {
   <S, A, B>(
